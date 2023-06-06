9850eebccc446ec25e007887415e7d0148726e49
Lucas Sinke <lsinke@student.codam.nl>
2023-06-06 19:01:17 +0200

    Add error checking for events returned by kevent

--- srcs/io/EventQueue.cc
+++ srcs/io/EventQueue.cc
@@ -97,6 +97,14 @@ EventQueue::Data* EventQueue::getUserData(const EventQueue::event& ev) {
 #endif
 }
 
+bool EventQueue::isError(const EventQueue::event& ev) {
+#ifdef __linux__
+  return false;
+#else
+  return ev.flags == EV_ERROR;
+#endif
+}
+
 void EventQueue::Data::operator()() {
   (void)handler;
   // handler.doIO();
