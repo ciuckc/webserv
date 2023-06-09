f72651a34c240b97dd3495ad7184504c13094bec
Mathijs Batstra <mbatstra@student.codam.nl>
2023-06-09 19:02:17 +0200

    it won't push

---
+++ CasesGET.cpp
@@ -0,0 +1,17 @@
+#include "CasesGET.h"
+
+CasesGET::CasesGET()
+{
+}
+
+CasesGET::CasesGET(const CasesGET&)
+{
+}
+
+CasesGET::~CasesGET()
+{
+}
+
+CasesGET& CasesGET::operator=(const CasesGET&)
+{
+}
---
+++ CasesGET.h
@@ -0,0 +1,14 @@
+#ifndef CASESGET_H
+#define CASESGET_H
+
+class CasesGET {
+  public:
+    CasesGET();
+    CasesGET(const CasesGET&);
+    ~CasesGET();
+    CasesGET& operator=(const CasesGET&);
+
+  private:
+};
+
+#endif
