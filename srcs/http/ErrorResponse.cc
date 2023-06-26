dd7b7603364760c24ef4fcf7327234fb89808e55
Lucas Sinke <lsinke@student.codam.nl>
2023-06-26 03:27:07 +0200

    Make err response thing constexpr

--- srcs/http/ErrorResponse.cc
+++ srcs/http/ErrorResponse.cc
@@ -4,18 +4,13 @@
 
 #include "Status.h"
 
-const std::string& ErrorResponse::errpage_template =
-    "<!DOCTYPE html>"
-    "<html><head><title>%d %s</title></head>"
-    "<body><h1>%s</h1><p>(Google what it means yourself)</p></body></html>";
-
 ErrorResponse::ErrorResponse(int error) noexcept {
   setMessage(error);
   addHeader("server", "webserv");
   addHeader("content-type", "text/html");
 
   std::string reason = http::getStatus(error);
-  size_t len = errpage_template.length() + (reason.length() * 2) + 3 - 6;
+  size_t len = strlen(errpage_template) + (reason.length() * 2) + 3 - 6;
   {
     std::stringstream str;
     str << len;
@@ -24,7 +19,7 @@ ErrorResponse::ErrorResponse(int error) noexcept {
   // todo: lazy initialize body when possible (while writing to socket)
 
   char* body = new char[len + 1];
-  std::snprintf(body, len + 1, errpage_template.c_str(), error, reason.c_str(), reason.c_str());
+  std::snprintf(body, len + 1, errpage_template, error, reason.c_str(), reason.c_str());
   setBody(body, len);
 }
 
