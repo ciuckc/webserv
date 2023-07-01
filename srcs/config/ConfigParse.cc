cc16dbf1dc852c3f1cc037b90ce872e521cbe490
scristia <scristi>
2023-07-01 23:54:16 +0200

    changed the tokenizer lexer function to return by value not by reference to parameter

--- srcs/config/ConfigParse.cc
+++ srcs/config/ConfigParse.cc
@@ -1,35 +1,30 @@
 #include "ConfigParse.h"
 
-#include <sys/_types/_size_t.h>
-
 #include <algorithm>
 #include <exception>
 #include <iostream>
-#include <list>
 #include <sstream>
 #include <string>
 
 namespace parse_utils {
-enum {
-  EMPTY
-};
-void split_on_white_space(std::ifstream& config_file, std::vector<std::string>& tokens);
-void split_on_symbols(std::vector<std::string>& tokens);
+enum { EMPTY };
+std::vector<std::string> split_on_white_space(std::ifstream& config_file);
+std::vector<std::string> split_on_symbols(const std::vector<std::string>& tokens);
 }  // namespace parse_utils
 
 ConfigParse::ConfigParse() {}
 
-ConfigParse::ConfigParse(const std::string& file_name_)
-    : config_file_(file_name_.c_str(), std::ios_base::in) {
+ConfigParse::ConfigParse(const std::string& file_name_) : config_file_(file_name_.c_str(), std::ios_base::in) {
   if (!config_file_.is_open())
     throw std::exception();
 }
 
 ConfigParse::~ConfigParse() {}
 
-void parse_utils::split_on_white_space(std::ifstream& config_file, std::vector<std::string>& tokens) {
+std::vector<std::string> parse_utils::split_on_white_space(std::ifstream& config_file) {
   std::string buffer;
   std::string token;
+  std::vector<std::string> tokens;
   size_t start_idx = 0;
   size_t end_idx = 0;
 
@@ -51,54 +46,46 @@ void parse_utils::split_on_white_space(std::ifstream& config_file, std::vector<s
       tokens.push_back(token);
     }
   }
+  return tokens;
 }
 
-// We will split a string if we find delimiters and then erase that string or if
-// we don't have any delimiter on which we can split, we just continue
-// forward iterating
-void parse_utils::split_on_symbols(std::vector<std::string>& tokens) {
-  size_t symbol_idx = 0;
-  size_t start_idx = 0;
-  std::string word;
+std::vector<std::string> parse_utils::split_on_symbols(const std::vector<std::string>& tokens) {
+  std::vector<std::string> lexemes;
   const std::string delimiters = "{}=;";
-  auto it = tokens.begin();
 
-  while (it != tokens.end()) {
-    symbol_idx = it->find_first_of(delimiters);
+  for (auto it = tokens.begin(); it != tokens.end(); ++it) {
+    std::size_t symbol_idx = it->find_first_of(delimiters);
     if (symbol_idx == it->npos) {
-      ++it;
+      lexemes.push_back(*it);
       continue;
     }
-    start_idx = 0;
+
+    std::size_t start_idx = 0;
     while (symbol_idx != it->npos) {
       if (start_idx < symbol_idx) {
-        word = it->substr(start_idx, symbol_idx - start_idx);
-        tokens.insert(it, word);
+        lexemes.push_back(it->substr(start_idx, symbol_idx - start_idx));
       }
-      word = it->substr(symbol_idx, 1);
-      tokens.insert(it, word);
+      lexemes.push_back(it->substr(symbol_idx, 1));
       start_idx = symbol_idx + 1;
       symbol_idx = it->find_first_of(delimiters, start_idx);
     }
     if (start_idx < it->length()) {
-      word = it->substr(start_idx);
-      tokens.insert(it, word);
+      lexemes.push_back(it->substr(start_idx));
     }
-    it = tokens.erase(it);
   }
+  return lexemes;
 }
 
 bool ConfigParse::parse(Config& config) {
   (void)config;
-  std::vector<std::string> tokens;
-  parse_utils::split_on_white_space(config_file_, tokens);
+  std::vector<std::string> tokens = parse_utils::split_on_white_space(config_file_);
   if (tokens.size() == parse_utils::EMPTY) {
     std::cout << "Configuration file is empty!" << std::endl;
     exit(1);
   }
+  tokens = parse_utils::split_on_symbols(tokens);
   for (auto it = tokens.begin(); it != tokens.end(); ++it) {
     std::cout << *it << std::endl;
   }
-  parse_utils::split_on_symbols(tokens);
   return true;
 }
