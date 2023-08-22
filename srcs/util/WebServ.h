18851171d5cb5b787fbc5853ba03e8aefc1585c3
Lucas Sinke <lsinke@student.codam.nl>
2023-08-22 19:00:00 +0200

    Use correct size for date header

--- srcs/util/WebServ.h
+++ srcs/util/WebServ.h
@@ -30,10 +30,10 @@ static constexpr uint32_t max_requests = 10;
 
 static inline std::string get_date_header() {
   // strlen("date: Mon, 01 Jan 0000 00:00:00 GMT\r\n") + 1;
-  static constexpr size_t date_header_len = 38;
+  static constexpr size_t date_header_len = 37;
   char out[date_header_len] = {};
   time_t time = std::time(nullptr);
-  std::strftime(out, date_header_len, "date: %a, %d %b %Y %T %Z\r\n", gmtime(&time));
+  std::strftime(out, date_header_len, "date: %a, %d %b %Y %T GMT\r\n", gmtime(&time));
   return { out, date_header_len };
 }
 }  // namespace WS
