d419caa15e74c0cbdabaeb75970af4b69a793152
Lucas Sinke <lsinke@student.codam.nl>
2023-09-13 15:49:55 +0200

    Remove kqueue support, add filter details to event logging

--- srcs/io/EventQueue.cc
+++ srcs/io/EventQueue.cc
@@ -1,14 +1,12 @@
 #include "EventQueue.h"
 
-#include <unistd.h>
-
 #include <cerrno>
 
 #include "IOException.h"
 #include "Server.h"
 
 EventQueue::EventQueue() : events_(), event_count_(), event_index_() {
-  queue_fd_ = Platform::create_queue();
+  queue_fd_ = epoll_create1(EPOLL_CLOEXEC);
   if (queue_fd_ == -1)
     throw IOException("Failed to create system event queue", errno);
 }
@@ -17,25 +15,33 @@ EventQueue::~EventQueue() {
   close(queue_fd_);
 }
 
-void EventQueue::add(int fd, filt_t direction) {
-  Platform::add(*this, fd, direction);
+void EventQueue::add(int fd, filt_t direction) const {
+  event_t event = create_event(fd, direction);
+  if (epoll_ctl(queue_fd_, EPOLL_CTL_ADD, fd, &event) == -1)
+    throw IOException("epoll add", errno);
 }
 
-void EventQueue::mod(int fd, filt_t new_direction) {
-  Platform::mod(*this, fd, new_direction);
+void EventQueue::mod(int fd, filt_t new_direction) const {
+  struct epoll_event event = create_event(fd, new_direction);
+  if (epoll_ctl(queue_fd_, EPOLL_CTL_MOD, fd, &event) == -1)
+    throw IOException("epoll mod", errno);
 }
 
-void EventQueue::del(int fd, filt_t dir) {
-  Platform::del(*this, fd, dir);
+void EventQueue::del(int fd) const {
+  epoll_ctl(queue_fd_, EPOLL_CTL_DEL, fd, nullptr);
 }
 
 EventQueue::event_t& EventQueue::getNext(Server& server) {
   while (event_index_ >= event_count_) {
     server.purge_connections(); // Purge every time we collect new events from the queue
     event_index_ = 0;
-    Platform::wait(*this);
-    if (event_count_ == -1)
+    event_count_ = epoll_wait(queue_fd_, events_, max_events, 5000);
+    if (event_count_ == -1 && errno != EINTR) // Ignore interrupts, don't care if we got signaled
       throw IOException("EventQueue", errno);
   }
   return events_[event_index_++];
+}
+
+EventQueue::event_t EventQueue::create_event(int fd, EventQueue::filt_t direction) {
+  return {.events = direction | r_hup, .data = { .fd = fd } };
 }
