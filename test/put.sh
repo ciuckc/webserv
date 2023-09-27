e33342616357c572adcaf987c87f4fc10e74e65d
Lucas Sinke <lsinke@student.codam.nl>
2023-09-27 15:12:33 +0200

    Add script to easily execute a put request

---
+++ test/put.sh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+
+if [[ $# -lt 2 ]]; then
+	echo "Usage: put.sh <port> <uri> [file]"
+	exit 1
+elif [[ $# -eq 2 ]]; then
+	size=0
+else
+	size=$(stat -c '%s' $3)
+fi
+
+port=$1
+uri=$2
+file=$3
+
+(cat << eof; [ -z "${file:-}" ] || cat $file) | nc localhost $port
+PUT $uri HTTP/1.1
+Host: localhost:$port
+Connection: close
+Content-length: $size
+
+eof
